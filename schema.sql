3457070c7874b0899503df7c9200eeb4718ab82e
Renan Fortunato <renanfortunato09@gmail.com>
2026-02-01 11:19:05 -0300

    chore: add create table products

--- schema.sql
+++ schema.sql
@@ -17,3 +17,14 @@ INSERT INTO examples (id, description, created_at, updated_at)
 VALUES 
     ('550e8400-e29b-41d4-a716-446655440000', 'First example', NOW(), NOW()),
     ('650e8400-e29b-41d4-a716-446655440001', 'Second example', NOW(), NOW());
+
+-- Products table
+CREATE TABLE IF NOT EXISTS products (
+    id VARCHAR(40) PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description TEXT NOT NULL,
+    price DECIMAL(10,2),
+    stock INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
